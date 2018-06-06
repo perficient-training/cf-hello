f7cab80a56b9b4792da4403e8388f2119ead8e11
Gordon Force <gordon.force@perficient.com>
2018-06-06 09:11:11 -0700

    Hello app deployable to Clould Foundry

---
+++ src/main/java/com/perficient/training/cf/hello/CfHelloApplication.java
@@ -0,0 +1,11 @@
+package com.perficient.training.cf.hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CfHelloApplication {
+    public static void main(String ...args) {
+        SpringApplication.run(CfHelloApplication.class, args);
+    }
+}
